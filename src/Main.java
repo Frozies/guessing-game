ca1cd20b638ffc2abcae2a1ecb89990a3ab09fca
Frozies <52686922+Frozies@users.noreply.github.com>
2021-02-15 23:30:11 -0500

    Reformated a few things. Need to calculate a score.

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,6 @@
 * Project started 2/15/2021
 * COP 2006
 *
-* PHASE 0:
-* Create a random number 1-15
-* TODO:
 * TODO: calculate a score on attempts array
 */
 
@@ -17,12 +14,9 @@ public class Main {
     public static void main(String[] args) {
         // Create a random number 1-15
         int randNumber = (int) (Math.random()*15);
-        int userGuesses[];
-        float userGuessAccuracy;
 
         // Start the program by asking the user for input and comparing the guess to the randomly generated number.
-        compareGuess(getUserInput(),randNumber);
-
+        compareGuess(getUserInput(), randNumber);
     }
 
     // This method asks for a user input for an integer. Returns the integer.
@@ -33,15 +27,18 @@ public class Main {
     }
 
     // This method compares the user input and the randomly generated number.
-    static void compareGuess(int currentGuess, int answer) {
-        if(currentGuess == answer) {
+    static void compareGuess(int currentGuess, int randNumber) {
+
+        if(currentGuess == randNumber) {
             System.out.println("You guessed correctly!!");
-        } else if(currentGuess>answer) {
+
+        } else if(currentGuess>randNumber) {
             System.out.println("Your guess was too high! Try again.");
-            compareGuess(getUserInput(),answer); // A loop to keep trying
-        } else if (currentGuess<answer) {
+            compareGuess(getUserInput(),randNumber); // A loop to keep trying
+
+        } else if (currentGuess<randNumber) {
             System.out.println("Your guess was too low! Try again.");
-            compareGuess(getUserInput(),answer); // A loop to keep trying
+            compareGuess(getUserInput(),randNumber); // A loop to keep trying
         }
         return;
     }
